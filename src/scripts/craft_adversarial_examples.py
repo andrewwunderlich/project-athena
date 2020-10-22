eb2f70f917c98c60776d522463bad96ff5f60bb0
Andrew <andreww@email.sc.edu>
2020-10-22 00:10:17 -0400

    added method and code to evaluate AEs with several defenses for Task 1

--- src/scripts/craft_adversarial_examples.py
+++ src/scripts/craft_adversarial_examples.py
@@ -15,9 +15,10 @@ import time
 from matplotlib import pyplot as plt
 import scipy.io
 
-from utils.model import load_lenet
+from utils.model import load_pool, load_lenet
+from models.athena import Ensemble, ENSEMBLE_STRATEGY
 from utils.file import load_from_json
-from utils.metrics import error_rate
+from utils.metrics import error_rate, get_corrections
 from attacks.attack import generate
 
 
@@ -96,6 +97,102 @@ def generate_ae(model, data, labels, attack_configs, save=False, output_dir=None
         print("Saving dataTable to "+file)
         #np.save(file, dataTable)
         scipy.io.savemat(file, {'dataTable':dataTable})
+        
+def evaluate(trans_configs, model_configs,
+             data_configs, labels, save=False, output_dir=None):
+    """
+    Apply transformation(s) on images.
+    :param trans_configs: dictionary. The collection of the parameterized transformations to test.
+        in the form of
+        { configsx: {
+            param: value,
+            }
+        }
+        The key of a configuration is 'configs'x, where 'x' is the id of corresponding weak defense.
+    :param model_configs:  dictionary. Defines model related information.
+        Such as, location, the undefended model, the file format, etc.
+    :param data_configs: dictionary. Defines data related information.
+        Such as, location, the file for the true labels, the file for the benign samples,
+        the files for the adversarial examples, etc.
+    :param labels: the correct labels for each image
+    :param save: boolean. Save the transformed sample or not.
+    :param output_dir: path or str. The location to store the transformed samples.
+        It cannot be None when save is True.
+    :return:
+    """
+    # Load the baseline defense (PGD-ADT model)
+    pgd_adt = load_lenet(file=model_configs.get('pgd_trained'), trans_configs=None,
+                                  use_logits=False, wrap=False)
+
+    # get the undefended model (UM)
+    file = os.path.join(model_configs.get('dir'), model_configs.get('um_file'))
+    undefended = load_lenet(file=file,
+                            trans_configs=trans_configs.get('configs0'),
+                            wrap=True)
+    print(">>> um:", type(undefended))
+
+    # load weak defenses into a pool
+    pool, _ = load_pool(trans_configs=trans_configs,
+                        model_configs=model_configs,
+                        active_list=True,
+                        wrap=True)
+    # create AVEP ensemble from the WD pool
+    wds = list(pool.values())
+    print(">>> wds:", type(wds), type(wds[0]))
+    ensemble_AVEP = Ensemble(classifiers=wds, strategy=ENSEMBLE_STRATEGY.AVEP.value)
+
+
+    # load the benign samples
+    bs_file = os.path.join(data_configs.get('dir'), data_configs.get('bs_file'))
+    x_bs = np.load(bs_file)
+    img_rows, img_cols = x_bs.shape[1], x_bs.shape[2]
+
+    ''' # this section replaced by passing in labels as a parameter
+    # load the corresponding true labels
+    label_file = os.path.join(data_configs.get('dir'), data_configs.get('label_file'))
+    labels = np.load(label_file)
+    if len(labels.shape) > 1:
+        labels = [np.argmax(p) for p in labels] #returns correct label
+    '''
+    # get indices of benign samples that are correctly classified by the targeted model
+    print(">>> Evaluating UM on [{}], it may take a while...".format(bs_file))
+    pred_bs = undefended.predict(x_bs)
+    corrections = get_corrections(y_pred=pred_bs, y_true=labels)
+
+    # Evaluate AEs.
+    results = {}
+    ae_list = data_configs.get('ae_files')
+
+    for ae_count in range(len(ae_list)): # step through ae's one by one
+        ae_file = os.path.join(data_configs.get('dir'), ae_list[ae_count])
+        x_adv = np.load(ae_file)
+
+        # evaluate the undefended model on the AE
+        print(">>> Evaluating UM on [{}], it may take a while...".format(ae_file))
+        pred_adv_um = undefended.predict(x_adv) #num_images by 10 array
+        pred_adv_um = [np.argmax(p) for p in pred_adv_um] # returns prediction
+        #err_um = error_rate(y_pred=pred_adv_um, y_true=labels, correct_on_bs=corrections)
+        # track the result
+        #results['UM'] = err_um
+
+        # evaluate the ensemble on the AE
+        print(">>> Evaluating ensemble on [{}], it may take a while...".format(ae_file))
+        pred_adv_AVEP = ensemble_AVEP.predict(x_adv)
+        pred_adv_AVEP = [np.argmax(p) for p in pred_adv_AVEP]
+        #err_AVEP = error_rate(y_pred=pred_adv_ens, y_true=labels, correct_on_bs=corrections)
+        # track the result
+        #results['Ensemble'] = err_AVEP
+
+        # evaluate the baseline on the AE
+        print(">>> Evaluating baseline model on [{}], it may take a while...".format(ae_file))
+        pred_adv_pgd_adt = pgd_adt.predict(x_adv)
+        pred_adv_pgd_adt = [np.argmax(p) for p in pred_adv_pgd_adt]
+        #err_pgd_adt = error_rate(y_pred=pred_adv_pgd_adt, y_true=labels, correct_on_bs=corrections)
+        # track the result
+        #results['PGD-ADT'] = err_pgd_adt
+
+    # TODO: collect and dump the evaluation results to file(s) such that you can analyze them later.
+    print(">>> Evaluations on [{}]:\n{}".format(ae_file, results))
 
 
 if __name__ == '__main__':
@@ -107,6 +204,9 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--model-configs', required=False,
                         default='configs/experiment/model-mnist.json',
                         help='Folder where models are stored.')
+    parser.add_argument('-t', '--trans-configs', required=False,
+                        default='configs/demo/athena-mnist.json',
+                        help='Configuration file for transformations.')
     parser.add_argument('-d', '--data-configs', required=False,
                         default='configs/experiment/data-mnist.json',
                         help='Folder where test data are stored.')
@@ -121,6 +221,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     print("------AUGMENT SUMMARY-------")
+    print('TRANSFORMATION CONFIGS:', args.trans_configs)
     print("MODEL CONFIGS:", args.model_configs)
     print("DATA CONFIGS:", args.data_configs)
     print("ATTACK CONFIGS:", args.attack_configs)
@@ -132,6 +233,7 @@ if __name__ == '__main__':
     model_configs = load_from_json(args.model_configs)
     data_configs = load_from_json(args.data_configs)
     attack_configs = load_from_json(args.attack_configs)
+    trans_configs = load_from_json(args.trans_configs)
 
     # load the targeted model
     model_file = os.path.join(model_configs.get("dir"), model_configs.get("um_file"))
@@ -145,9 +247,19 @@ if __name__ == '__main__':
     labels = np.load(label_file)
 
     # generate adversarial examples 
-    num_images = 51 #set to large number (maybe 1000) for final run, <50 while developing for speed
+    num_images = 15 #set to large number (maybe 1000) for final run, <50 while developing for speed
     data_bs = data_bs[:num_images]
     labels = labels[:num_images]
-    generate_ae(model=target, data=data_bs, labels=labels, attack_configs=attack_configs,
-                save=True, output_dir=('C:/Users/andre/CSCE585_local/'+
-                                       'project-athena/saved_attacks'))
+    '''generate_ae(model=target, 
+                data=data_bs, 
+                labels=labels, 
+                attack_configs=attack_configs,
+                save=False, output_dir=('C:/Users/andre/CSCE585_local/'+
+                                       'project-athena/data'))
+    '''
+    evaluate(trans_configs=trans_configs,
+             model_configs=model_configs,
+             data_configs=data_configs,
+             labels = labels
+             save=False,
+             output_dir=args.output_root)
